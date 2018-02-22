99a241b9352ad64a1ace578d3ed37755f97003b9
Louis LeBohec <louisblebohec@gmail.com>
2018-02-22 14:58:15 -0700

    panic commit due to VStudio not working anymore

---
+++ ClientApp/src/assets/ajax_query.js
@@ -0,0 +1,17 @@
+﻿
+
+
+$("#search_submit").on("click", function(){
+
+    var search_term = $("#search_term").val();
+    console.log(search_term);
+
+    $.AJAX({
+        type: 'GET',
+        url: ""
+    })
+});
+   
+
+
+
--- Controllers/db_query.cs
+++ Controllers/db_query.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+
 namespace EmpSearch_v2.Controllers
 {
     public class db_query
     {
-        public db_query()
+        static string test()
         {
+            string test = "it fuckin worked motherfucker!!!";
+
+            return test;
         }
     }
 }
