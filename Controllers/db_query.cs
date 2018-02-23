c5a71d11b08e39e7063b8dc76d5ce98806dda9a2
Louis LeBohec <louisblebohec@gmail.com>
2018-02-23 10:08:01 -0700

    semi-final commit. Everything works, in theory, except 404 Not Found error on Ajax call

--- Controllers/db_query.cs
+++ Controllers/db_query.cs
@@ -19,6 +19,7 @@ namespace EmpSearch_v2.Controllers
             // NOTE: This is vulnerable to SQL Injection: I'm aware of this issue, not sure how to prevent it. I don't think there's a way
             // to detect if a string is code in a certain language.
             string query = "SELECT * FROM emps WHERE name LIKE '%"+searchterm+"%'";
+            string results = "";
             
             
                 sQLite.Open();
@@ -30,16 +31,31 @@ namespace EmpSearch_v2.Controllers
 
                     // I will format DB results into HTML, stuff it in a string, and return to the JS, to
                     // then be inserted into the display
-                    string results = "";
-
+                    
+                    if(reader.HasRows){
                     // This while loop with the DataReader function is super convenient. 
                     while(reader.Read()){
+                        // temporary variable to store formatted HTML, before being concatted to results string
+                        string temp_html_builder = "";
                         // console.log as test.
                         Console.WriteLine("ID: "+reader["id"]+" || "+reader["name"]);
+
+                        temp_html_builder = "<div class='row'>"+
+                        "<div class='col-md-3'><h4>"+reader["id"]+"</h4></div>"+
+                        "<div class='col-md-3'><h4>"+reader["name"]+"</h4></div>"+
+                        "</div>";
+
+                        results+=temp_html_builder;
+
+
+                    }
+                    }
+                    else{
+                        results = "<div class='row'><div class='col-md-6'><h4>There were no results</h4></div</div>";
                     }
                 }
 
-            return query;
+            return results;
         }
     }
 }
