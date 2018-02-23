5f54c0804f9d88b3f42d715fcb9f80bc6b878e59
Louis LeBohec <louisblebohec@gmail.com>
2018-02-23 08:39:00 -0700

    progress commit, also added comments to AJAX and controller files

--- Controllers/db_query.cs
+++ Controllers/db_query.cs
@@ -8,10 +8,38 @@ namespace EmpSearch_v2.Controllers
     
     public class db_query
     {
-        public string db_search(string search_term){
-            string res = search_term;
+        private SQLiteConnection sQLite;
 
-            return res;
+        public void DataClass(){
+            sQLite = new SQLiteConnection("Data Source = emps.db");
+        }
+        public string db_search(string searchterm){
+            
+            // Inserts search term into query
+            // NOTE: This is vulnerable to SQL Injection: I'm aware of this issue, not sure how to prevent it. I don't think there's a way
+            // to detect if a string is code in a certain language.
+            string query = "SELECT * FROM emps WHERE name LIKE '%"+searchterm+"%'";
+            
+            
+                sQLite.Open();
+                
+
+                using(sQLite){
+                    SQLiteCommand command = new SQLiteCommand(query, sQLite);
+                    SQLiteDataReader reader = command.ExecuteReader();
+
+                    // I will format DB results into HTML, stuff it in a string, and return to the JS, to
+                    // then be inserted into the display
+                    string results = "";
+
+                    // This while loop with the DataReader function is super convenient. 
+                    while(reader.Read()){
+                        // console.log as test.
+                        Console.WriteLine("ID: "+reader["id"]+" || "+reader["name"]);
+                    }
+                }
+
+            return query;
         }
     }
 }
